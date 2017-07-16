72c37b830a04a11a25f7aa4c27058cf2b9e42258
Arve Hansen <arveh4@outlook.com>
2017-07-16 18:46:44 +0200

    Simple Activity with just logging.

---
+++ MoveBlobCustomActivity/MoveBlobCustomActivity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories.Runtime;
+
+namespace MoveBlobCustomActivityNS
+{
+    public class MoveBlobCustomActivity: IDotNetActivity
+    {
+        public IDictionary<string, string> Execute(IEnumerable<LinkedService> linkedServices, IEnumerable<Dataset> datasets, Activity activity, IActivityLogger logger)
+        {
+            logger.Write("\nAll Linked Services(s) Below ");
+            foreach (LinkedService ls in linkedServices)
+                logger.Write("\nLinked Service: " + ls.Name);
+
+            logger.Write("\nAll Dataset(s) Below ");
+            foreach (Dataset ds in datasets)
+                logger.Write("\n\tDataset: " + ds.Name);
+
+            return new Dictionary<string, string>();
+        }
+    }
+}
