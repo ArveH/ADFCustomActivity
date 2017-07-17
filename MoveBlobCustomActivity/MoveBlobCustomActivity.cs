ca87fcbce81f0f97ec659fe3243e6823bbbd67cd
Arve Hansen <arveh4@outlook.com>
2017-07-17 12:46:34 +0200

    Startet rewrite

--- MoveBlobCustomActivity/MoveBlobCustomActivity.cs
+++ MoveBlobCustomActivity/MoveBlobCustomActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Azure.Management.DataFactories.Models;
@@ -18,47 +19,18 @@ namespace MoveBlobCustomActivityNS
         {
             try
             {
-                DotNetActivity dotNetActivity = (DotNetActivity)activity.TypeProperties;
+                LogDataFactoryElements(linkedServices, datasets, activity, logger);
 
-                IDictionary<string, string> extendedProperties = dotNetActivity.ExtendedProperties;
-                logger.Write("\nLogging extended properties if any...");
-                foreach (KeyValuePair<string, string> entry in extendedProperties)
-                {
-                    logger.Write("\n\t<key:{0}> <value:{1}>", entry.Key, entry.Value);
-                }
+                var inputDataSet = GetInputDataset(datasets, activity);
+                var inputLinkedService = GetInputLinkedService(linkedServices, inputDataSet);
 
-                var inputToDelete = dotNetActivity.ExtendedProperties["InputToDelete"];
-                logger.Write("\nInput to delete is " + inputToDelete);
+                CloudBlobClient blobStorageClient = GetBlobStorageClient(
+                    inputLinkedService,
+                    logger);
 
-                logger.Write("\nAll Linked Services(s) Below ");
-                foreach (LinkedService ls in linkedServices)
-                    logger.Write("\n\tLinked Service: " + ls.Name);
-
-                logger.Write("\nAll Dataset(s) Below ");
-                foreach (Dataset ds in datasets)
-                    logger.Write("\n\tDataset: " + ds.Name);
-
-                foreach (string name in activity.Inputs.Select(i => i.Name))
-                {
-                    logger.Write("\nInput Dataset: " + name);
-                }
-
-                foreach (string name in activity.Outputs.Select(i => i.Name))
-                {
-                    logger.Write("\nOutput Dataset: " + name);
-                }
-
-                if (!dotNetActivity.ExtendedProperties.ContainsKey("InputToDelete"))
-                {
-                    throw new Exception("Extended property 'InputToDelete' is missing. Add it to \"extendedProperties\" in the pipeline definition.");
-                }
-
-                List<string> dataSetsToDelete = inputToDelete.Split(',').ToList();
-
-                DeleteBlobFileFolder(
-                    linkedServices,
-                    datasets,
-                    dataSetsToDelete,
+                DeleteBlobs(
+                    blobStorageClient,
+                    inputDataSet.Properties.TypeProperties as AzureBlobDataset,
                     logger);
 
                 logger.Write("Custom Activity Ended Successfully.");
@@ -74,6 +46,125 @@ namespace MoveBlobCustomActivityNS
             return new Dictionary<string, string>();
         }
 
+        private static Dataset GetInputDataset(IEnumerable<Dataset> datasets, Activity activity)
+        {
+            return datasets.First(ds => ds.Name == activity.Inputs.First().Name);
+        }
+
+        private static AzureStorageLinkedService GetInputLinkedService(
+            IEnumerable<LinkedService> linkedServices,
+            Dataset inputDataset)
+        {
+            return
+                linkedServices
+                    .First(ls => ls.Name == inputDataset.Properties.LinkedServiceName)
+                    .Properties
+                    .TypeProperties as AzureStorageLinkedService;
+        }
+
+        private static CloudBlobClient GetBlobStorageClient(
+            AzureStorageLinkedService inputLinkedService,
+            IActivityLogger logger)
+        {
+            if (string.IsNullOrWhiteSpace(inputLinkedService.ConnectionString))
+            {
+                var msg = "Connection string is empty";
+                logger.Write(msg);
+                throw new Exception(msg);
+            }
+
+            CloudBlobClient client;
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(inputLinkedService.ConnectionString);
+                client = storageAccount.CreateCloudBlobClient();
+                client.ListContainers(); // Just making sure it works
+            }
+            catch (Exception ex)
+            {
+                var msg = "Couldn't create CloudBlobClient";
+                logger.Write(msg);
+                throw new Exception(msg, ex);
+            }
+
+            logger.Write("Created CloudBlobClient");
+            return client;
+        }
+
+        private static void LogDataFactoryElements(IEnumerable<LinkedService> linkedServices, IEnumerable<Dataset> datasets, Activity activity,
+            IActivityLogger logger)
+        {
+            DotNetActivity dotNetActivity = (DotNetActivity) activity.TypeProperties;
+
+            IDictionary<string, string> extendedProperties = dotNetActivity.ExtendedProperties;
+            logger.Write("\nLogging extended properties if any...");
+            foreach (KeyValuePair<string, string> entry in extendedProperties)
+            {
+                logger.Write("\n\t<key:{0}> <value:{1}>", entry.Key, entry.Value);
+            }
+
+            logger.Write("\nAll Linked Services(s) Below ");
+            foreach (LinkedService ls in linkedServices)
+                logger.Write("\n\tLinked Service: " + ls.Name);
+
+            logger.Write("\nAll Dataset(s) Below ");
+            foreach (Dataset ds in datasets)
+                logger.Write("\n\tDataset: " + ds.Name);
+
+            foreach (string name in activity.Inputs.Select(i => i.Name))
+            {
+                logger.Write("\nInput Dataset: " + name);
+            }
+
+            foreach (string name in activity.Outputs.Select(i => i.Name))
+            {
+                logger.Write("\nOutput Dataset: " + name);
+            }
+        }
+
+        private void DeleteBlobs(
+            CloudBlobClient blobStorageClient, 
+            AzureBlobDataset blobDataset, 
+            IActivityLogger logger)
+        {
+            LogBlobDataSetInfo(blobDataset, logger);
+
+            var folderPath = blobDataset.FolderPath;
+
+            var blobs = blobStorageClient.ListBlobs(folderPath, true).ToList();
+            logger.Write($"Found {blobs.Count}");
+        }
+
+        private static void LogBlobDataSetInfo(AzureBlobDataset blobDataset, IActivityLogger logger)
+        {
+            logger.Write("\nBlob folder: " + blobDataset.FolderPath);
+            logger.Write("\nBlob format: " + blobDataset.Format);
+
+            var partitions = blobDataset.PartitionedBy?.Count ?? 0;
+            logger.Write($"\nPartitions ({partitions}):");
+            for (int i = 0; i < partitions; i++)
+            {
+                logger.Write($"\n\t{blobDataset.PartitionedBy?[i].Name ?? "null"}: {blobDataset.PartitionedBy?[i]?.Value}");
+            }
+
+            logger.Write("\nBlob file: " + blobDataset.FileName);
+
+            if (blobDataset.FolderPath.IndexOf("/", StringComparison.InvariantCulture) <= 0)
+            {
+                throw new Exception($"Can't find container name for dataset '{blobDataset.FolderPath}'");
+            }
+
+            string containerName =
+                blobDataset.FolderPath.Substring(0,
+                    blobDataset.FolderPath.IndexOf("/", StringComparison.InvariantCulture));
+            logger.Write("\nContainer Name {0}", containerName);
+
+            string directoryName =
+                blobDataset.FolderPath.Substring(
+                    blobDataset.FolderPath.IndexOf("/", StringComparison.InvariantCulture) + 1);
+            logger.Write("\nDirectory Name {0}", directoryName);
+        }
+
         public void DeleteBlobFileFolder(
             IEnumerable<LinkedService> linkedServices,
             IEnumerable<Dataset> datasets,
@@ -85,14 +176,20 @@ namespace MoveBlobCustomActivityNS
                 Dataset inputDataset = datasets.First(ds => ds.Name.Equals(strInputToDelete));
                 AzureBlobDataset blobDataset = inputDataset.Properties.TypeProperties as AzureBlobDataset;
                 logger.Write("\nBlob folder: " + blobDataset.FolderPath);
+                logger.Write("\nBlob format: " + blobDataset.Format);
+
+                logger.Write("\nPartitions (if any):");
+                foreach (var partition in blobDataset.PartitionedBy)
+                {
+                    logger.Write($"\n\t{partition.Name}: {partition.Value}");
+                }
                 logger.Write("\nBlob file: " + blobDataset.FileName);
 
-                // linked service for input and output is the same.
-                AzureStorageLinkedService linkedService = linkedServices.First(ls =>
+                AzureStorageLinkedService inputLinkedService = linkedServices.First(ls =>
                     ls.Name == inputDataset.Properties.LinkedServiceName).Properties.TypeProperties as AzureStorageLinkedService;
 
                 // create storage client for input. Pass the connection string.
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(linkedService.ConnectionString);
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(inputLinkedService.ConnectionString);
                 CloudBlobClient client = storageAccount.CreateCloudBlobClient();
 
                 // find blob to delete and delete if exists.
